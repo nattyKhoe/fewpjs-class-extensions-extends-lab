638cfaa2d881299811c02f46d956b01c2e340699
Natasha <natasha.kusuma@outlook.com>
2022-07-08 17:24:36 +1000

    commit tree bcc697fa371d9a6723791826191d3ba36f22355d

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Your code here
+class Polygon {
+    constructor (array) {
+        this.array = array;
+    }
+
+    get countSides () {
+        return this.array.length;
+    }
+
+    get perimeter () {
+        let perimeter = 0;
+
+        for(let i = 0; i<this.array.length; i++){
+            perimeter = perimeter+this.array[i];
+        }
+
+        return perimeter;
+    }
+}
+
+class Triangle extends Polygon {
+    constructor (array){
+        super (array);
+    }
+    
+    get isValid () {
+        let sides = this.array;
+        
+        if (this.countSides === 3){
+            if ((sides[0]+sides[1]>sides[2])&&(sides[1]+sides[2]>sides[0])&&(sides[0]+sides[2]>sides[1])){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+}
+
+class Square extends Polygon {
+    get isValid () {
+        let sides = this.array;
+
+        if (this.countSides === 4){
+            if (sides[0]===sides[1]&&sides[2]===sides[3]&&sides[0]===sides[2]){
+                return true;
+        //     } else {
+        //         return false;
+        //     }
+        // } else {
+        //     return false;
+        }
+    }
+    return false;
+}
+
+    get area () {
+        if (this.isValid === true) {
+            return this.array[0]**2;
+        } else {
+            return 'Not a square';
+        }
+    }
+}
